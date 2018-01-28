c7bed87eac66e59eb8b627bb5d93ad712a322b91
sakichat <saki28chat@gmail.com>
2018-01-27 23:55:50 -0500

    parse json

--- src/main/java/Receiver.java
+++ src/main/java/Receiver.java
@@ -1,11 +1,18 @@
+import com.ibm.watson.developer_cloud.language_translator.v2.LanguageTranslator;
+import com.ibm.watson.developer_cloud.language_translator.v2.model.TranslateOptions;
+import com.ibm.watson.developer_cloud.language_translator.v2.model.TranslationResult;
+import com.ibm.watson.developer_cloud.language_translator.v2.util.Language;
 import com.ibm.watson.developer_cloud.visual_recognition.v3.VisualRecognition;
-import com.ibm.watson.developer_cloud.visual_recognition.v3.model.ClassifiedImages;
-import com.ibm.watson.developer_cloud.visual_recognition.v3.model.ClassifyOptions;
+import com.ibm.watson.developer_cloud.visual_recognition.v3.model.*;
 import com.twilio.twiml.Body;
 import com.twilio.twiml.Message;
 import com.twilio.twiml.MessagingResponse;
+import spark.Request;
+import spark.Response;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static spark.Spark.post;
@@ -22,7 +29,7 @@ public class Receiver {
 
     public static void main(String[] args) {
 
-        post("receive-sms", (req, res) -> {
+        post("receive-sms", (Request req, Response res) -> {
 
             // receive the client message, put them into a map
             String body = req.body();
@@ -36,6 +43,8 @@ public class Receiver {
             }
 
             int numMedia = Integer.parseInt(map.get("NumMedia"));
+
+            String reply = null;
 
             // if it is an image
             if (numMedia > 0) {
@@ -57,28 +66,67 @@ public class Receiver {
                                         + "}")
                             .build();
                     ClassifiedImages result = service.classify(classifyOptions).execute();
-                    System.out.println(result);
+                    //System.out.println(result);
+                    List<ClassifiedImage> classifiedImagList = result.getImages();
+                    ClassifiedImage classifiedImage = classifiedImagList.get(0);
+                    List<ClassifierResult> classifierResultList = classifiedImage.getClassifiers();
+                    ClassifierResult classifierResult = classifierResultList.get(0);
+                    List<ClassResult> classResultList = classifierResult.getClasses();
+                    classResultList.sort((o1, o2) -> {
+                        if (o1.getScore() > o2.getScore()) return 1;
+                        return -1;
+                    });
+                    for (ClassResult cResult : classResultList) {
+                        if (!cResult.getClassName().contains("color")){
+                            reply = cResult.getClassName();
+                            break;
+                        }
+                    }
 
                     // handle the result and give response to client
                 }
             }
             // not an image
             else {
-                String message = "";
                 for(String string: map.keySet()){
                     if(string.equals("Body")){
-                        message = map.get(string).replaceAll("\\+"," ");
+                        reply = map.get(string).replaceAll("\\+"," ");
                         break;
                     }
                 }
-                System.out.println(message);
+
+                //System.out.println(message);
+
+
+                LanguageTranslator service = new LanguageTranslator();
+                service.setUsernameAndPassword("3349f689-0b9b-45db-8520-e41c3ea5d6df","T4kqlsaqLEAE");
+
+                ArrayList<String> arrayList = new ArrayList<>();
+//                arrayList.add(message);
+
+                TranslateOptions translateOptions = new TranslateOptions.Builder()
+                        .text(arrayList)
+                        .source(Language.ENGLISH)
+                        .target(Language.FRENCH)
+                        .build();
+
+                TranslationResult result = service.translate(translateOptions)
+                        .execute();
+
+                System.out.println(result);
+
+//                System.out.println(message);
+
             }
 
-            Message sms = new Message.Builder()
-                    .body(new Body("hello world"))
-                    .build();
-            MessagingResponse twiml = new MessagingResponse.Builder().message(sms).build();
-            return twiml.toXml();
+            if (reply != null){
+                Message sms = new Message.Builder()
+                        .body(new Body(reply))
+                        .build();
+                MessagingResponse twiml = new MessagingResponse.Builder().message(sms).build();
+                return twiml.toXml();
+            }
+            return null;
 
         });
     }
